4f424ec9cabd0473605048dc6ec42701c4f67c0c
Mojarrita <124315334+Mojarritaa@users.noreply.github.com>
2023-05-23 20:27:28 -0500

    Update Main.java

--- PROG ORIEN OBJ (POO)/JUEGOTALLER/Main.java
+++ PROG ORIEN OBJ (POO)/JUEGOTALLER/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Instanciar el objeto de tipo GameFrame
+        //Instanciar el objeto de tipo GameFrame, ya de por sí se va a ejecutar por su constructor mismo. 
         new GameFrame();
     }
 }

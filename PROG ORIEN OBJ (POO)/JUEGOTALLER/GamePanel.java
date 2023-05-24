b85cfc0602781af5fdf3d9b96390dea33abcc6cb
Mojarrita <124315334+Mojarritaa@users.noreply.github.com>
2023-05-23 20:26:57 -0500

    Update GamePanel.java

--- PROG ORIEN OBJ (POO)/JUEGOTALLER/GamePanel.java
+++ PROG ORIEN OBJ (POO)/JUEGOTALLER/GamePanel.java
@@ -1,3 +1,5 @@
+//Este código fue sacado de BroCode https://www.youtube.com/@BroCodez//
+//Importar librerías necesarias
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -30,6 +32,7 @@ public class GamePanel extends JPanel implements ActionListener {
     boolean running = false;
     Timer timer;
     Random random;
+ //Constructor, cada vez que se inicializa un GamePanel, iniciará el juego
    GamePanel(){
        random = new Random();
        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -38,25 +41,31 @@ public class GamePanel extends JPanel implements ActionListener {
        this.addKeyListener(new MyKeyAdapter());
        startGame();
    }
+ //Funcion que se encarga del inicio del juego, iniciliazar los atributos/parametros
    public void startGame(){
        newPoint();
        running = true;
        timer = new Timer(DELAY, this);
        timer.start();
    }
+ //Funcion que se encarga de imprimir en el panel a través del método draw
    public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
    }
+ //Funcion que se encarga de "imprimir" los contenidos en el panel, los dibuja
    public void draw(Graphics g) {
+    //Verificar que el juego siga corriendo, es decir, que el jugador no ha perdido
        if (running) {
+        //Imprimir rejillas para el movimiento, no son necesarias opcionales
            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_WIDTH);
                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
            }
+        //El color del punto/manzana
            g.setColor(Color.cyan);
            g.fillOval(pointX, pointY, UNIT_SIZE, UNIT_SIZE);
-
+        //Imprimir la serpiente, tanto la cabeza [0] y el resto del cuerpo
            for (int i = 0; i < bodyParts; i++) {
                if (i == 0) {
                    g.setColor(Color.GREEN);
@@ -66,24 +75,31 @@ public class GamePanel extends JPanel implements ActionListener {
                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                }
            }
+        //Mostrar en pantalla el marcador del jugador, su puntaje
            g.setColor(new Color(98,37,89));
            g.setFont(new Font("SansSerif", Font.BOLD, 30));
            FontMetrics metrics = getFontMetrics(g.getFont());
            g.drawString("Puntos: " + pointEaten, (SCREEN_WIDTH - metrics.stringWidth("Puntos: " + pointEaten))/2, g.getFont().getSize());
        }
        else{
+        //Si pierde, muestra en pantalla gameover, utiliza el metodo gameOver
            gameOver(g);
        }
    }
+ //Funcion que crea nuevas coordenadas para la manzana/punto
    public void newPoint(){
+    //Crear nuevas posiciones de la manzana/punto utilizando random
         pointX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
         pointY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
    }
+ //Función que se encarga del movimiento de la serpiente
    public void move(){
+    //Hace el intercambio
         for(int i = bodyParts; i>0; i--){
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
+    //Hace el cambio de la dirección
         switch (direction){
             case 'U' ->{
                 y[0] = y[0] - UNIT_SIZE;
@@ -99,10 +115,13 @@ public class GamePanel extends JPanel implements ActionListener {
             }
         }
    }
+ //verificar que la serpiente se haya comido la manzana/punto
    public void checkPoint(){
        if((x[0] == pointX) && (y[0]==pointY)){
+        //Agrandar la serpiente y aumentar el contador
            bodyParts++;
            pointEaten++;
+        //Crear una nueva manzana
            newPoint();
        }
    }
@@ -130,15 +149,18 @@ public class GamePanel extends JPanel implements ActionListener {
        if(y[0] > SCREEN_HEIGHT){
            running = false;
        }
+    //Si el jugador pierde, se para el timer
        if(!running){
            timer.stop();
        }
    }
+ //Funcion que se encarga de mostrar en pantalla el mensaje de Game over, si pierde el jugador
    public  void gameOver(Graphics g){
        //Mostrar el puntaje
        g.setColor(new Color(98,37,89));
        g.setFont(new Font("SansSerif", Font.BOLD, 30));
        FontMetrics metrics1 = getFontMetrics(g.getFont());
+    //Esto muestra el mensaje en la mitad del panel
        g.drawString("Puntos: " + pointEaten, (SCREEN_WIDTH - metrics1.stringWidth("Puntos: " + pointEaten))/2, g.getFont().getSize());
         //Mostrar que se acabo el juego
        g.setColor(Color.red);
@@ -147,7 +169,7 @@ public class GamePanel extends JPanel implements ActionListener {
       g.drawString("GAME OVER!!!!!!", (SCREEN_WIDTH - metrics2.stringWidth("GAME OVER!!!!!!"))/2, SCREEN_HEIGHT/2);
 
    }
-
+//Funcion que se encarga de la acción, maneja el juego
     @Override
     public void actionPerformed(ActionEvent e){
         if(running){
@@ -157,10 +179,12 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         repaint();
     }
+ //Recibe las teclas oprimidas por el jugador para poder mover la serpiente
     public class MyKeyAdapter extends KeyAdapter{
        @Override
         public void keyPressed(KeyEvent e){
            switch (e.getKeyCode()){
+             //Verificar que los movimientos se hagan correctamente y eviten chocarse al hacerlo, evitar un bug. 
                case KeyEvent.VK_LEFT -> {
                    if(direction != 'R'){
                        direction = 'L';
